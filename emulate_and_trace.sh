c399e6c8e92b9c7bc7aaa75e0d31d3383f2e38b3
SMCINNIS94 <spencer.mcinnis@outlook.com>
2025-06-04 12:08:47 -0400

    Run QEMU in background

--- emulate_and_trace.sh
+++ emulate_and_trace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-qemu-system-arm \
+setsid qemu-system-arm \
   -M virt \
   -cpu cortex-a15 \
   -kernel openwrt-22.03.5-armvirt-32-zImage \
@@ -10,5 +10,8 @@ qemu-system-arm \
   -fsdev local,id=host0,path=$(pwd),security_model=none \
   -device virtio-9p-device,fsdev=host0,mount_tag=hostshare \
   -netdev user,id=net0 \
-  -device virtio-net-device,netdev=net0
+  -device virtio-net-device,netdev=net0 \
+  </dev/null &>run.log &
+
+echo "QEMU started in the background. Console output is in run.log"
 
